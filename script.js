c07ac63501d45d83146dcdc79045774a382283c6
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-04 10:31:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the necessary elements
+  var colorSelect = document.getElementById("colorSelect");
+  var button = document.querySelector("input[type='button']");
+
+  // Add event listener to the button
+  button.addEventListener("click", function() {
+    // Get the selected option
+    var selectedOption = colorSelect.options[colorSelect.selectedIndex];
+
+    // Remove the selected option from the dropdown
+    colorSelect.removeChild(selectedOption);
+  });
+});
